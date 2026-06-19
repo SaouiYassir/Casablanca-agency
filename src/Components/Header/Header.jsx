2a623c106fea188f3e76b60a4965461c058973cb
SaouiYassir <yassirsaoui06@gmail.com>
2026-06-19 17:19:02 +0100

    Header Component V1

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import './Header.css'
+
+function Header() {
+    return (
+        <header className="header">
+            <div className="logo">Casablanca Location</div>
+            <div className="nav">
+                <a href="">Home</a>
+                <a href="">About</a>
+                <a href="">Contact</a>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
