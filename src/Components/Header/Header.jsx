b93f26c8b32db463d44931bbce6d388ddc4a9ebe
Saoui_Yassir <yassirsaoui06@gmail.com>
2026-06-22 16:16:49 +0100

    Header Responsive | Hamburger Menu Added

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,6 +3,7 @@ import './Header.css'
 
 function Header() {
     const [isScrolled, setIsScrolled] = useState(false);
+    const [isOpen, setIsOpen] = useState(false);
 
     useEffect(() => {
         const handleScroll = () => {
@@ -14,12 +15,16 @@ function Header() {
         };
 
         window.addEventListener('scroll', handleScroll);
-                
+
         return () => window.removeEventListener('scroll', handleScroll);
     }, []);
 
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
     return (
-    
+
         <header className={`header ${isScrolled ? 'scrolled' : ''}`}>
             <div className="logo">Casablanca Location</div>
             <nav className="nav">
@@ -27,6 +32,20 @@ function Header() {
                 <a href="#about">About</a>
                 <a href="/all-cars">Catalogue</a>
                 <a href="#contact">Contact</a>
+            </nav>
+
+
+            <div className={`hamburger ${isOpen ? 'active' : ''}`} onClick={toggleMenu}>
+                <span className="bar"></span>
+                <span className="bar"></span>
+                <span className="bar"></span>
+            </div>
+            
+            <nav className={`nav ${isOpen ? 'active' : ''}`}>
+                <a href="/" onClick={() => setIsOpen(false)}>Home</a>
+                <a href="#about" onClick={() => setIsOpen(false)}>About</a>
+                <a href="/all-cars" onClick={() => setIsOpen(false)}>Catalogue</a>
+                <a href="#contact" onClick={() => setIsOpen(false)}>Contact</a>
             </nav>
         </header>
     );
--- src/Pages/Home-Page/Home.jsx
+++ src/Pages/Home-Page/Home.jsx
@@ -15,7 +15,7 @@ function Home() {
                     <p>Your trusted partner for car rentals in Casablanca.</p>
                 </div>
                 <div className="hero-buttons">
-                    <button className="explore-btn">Explore The Catalog</button>
+                    <button className="explore-btn" onClick={()=> window.open('/all-cars', '_parent')}>Explore The Catalog</button>
                     <button className="contact-btn" onClick={() => window.open('https://wa.me/212601109965', '_blank')}>
                         <i className="bi bi-whatsapp"></i> Reserve Now
                     </button>
