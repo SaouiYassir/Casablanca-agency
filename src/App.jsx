8c79b2aa3d85662c8a5a760bea5e8ecb5592857f
Saoui_Yassir <yassirsaoui06@gmail.com>
2026-07-13 15:54:08 +0100

    Fixing The Route Problem

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,29 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Header from "./Components/Header/Header.jsx";
 import Home from './Pages/Home-Page/Home.jsx'
 import Catalogue from "./Pages/Catalogue/Catalogue.jsx";
-import './App.css'
 import About from './Pages/About/About.jsx';
 import Contact from './Pages/Contact/Contact.jsx';
 import Footer from './Components/Footer/Footer.jsx';
+import './App.css'
 
 function App() {
     return (
-        <Router>
-            <div className="app-layout">
-                <Header />
-                <main className="app-main">
-                    <Routes>
-                        <Route path="/" element={
-                            <>
-                                <Home />                        
-                            </>
-                        } />
+        <BrowserRouter>
+            <Header />
 
-                        <Route path="/all-cars" element={
-                            <div style={{ paddingTop: '120px' }}>
-                                <Catalogue page={true} />
-                            </div>
-                        } />
+            <Routes>
 
-                        <Route path='/about' element={
-                            <>
-                                <About />
-                            </>
-                        } />
+                <Route path="/" element={<Home />} />
+                <Route path="/all-cars" element={<Catalogue />} />
+                <Route path='/about' element={<About />}></Route>
+                <Route path='/contact' element={<Contact />}></Route>            
 
-                        <Route path='/contact' element={
-                            <>
-                                <Contact />
-                            </>
-                        } />
-                    </Routes>
-                </main>
-                <Footer />
-            </div>
-        </Router>
+            </Routes>
+
+            <Footer />
+        </BrowserRouter>
     );
 }
 
